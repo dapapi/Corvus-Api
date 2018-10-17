db7c2ff879eec0ae6cdeee58ac6ed41b91a19b49
wyjson <1033567022@qq.com>
2018-10-17 10:31:03 +0800

    del test code
    Signed-off-by: wyjson <1033567022@qq.com>

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,5 @@ $api->version('v1', ['middleware' => ['bindings', 'cors']], function ($api) {
         $api->get('/test/hello', 'App\Http\Controllers\TestController@hello');
 
         $api->post('/test/login', 'App\Http\Controllers\TestController@signin');
-        $api->get('/test/wechat_token', 'App\Http\Controllers\TestController@getWechatToken');
-        $api->get('/test/spider', 'App\Http\Controllers\TestController@spider');
-        $api->get('/test/spider_change', 'App\Http\Controllers\TestController@spiderChange');
     }
 });

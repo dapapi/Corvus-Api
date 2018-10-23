fcc83c130c93d1e6c8e4587a63671acaa92a58ec
OmegaPenguin <xlf_129@163.com>
2018-10-23 15:09:20 +0800

    add department tables

---
+++ database/migrations/2018_10_23_063617_create_department_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('department_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('department_id')
+                ->references('id')
+                ->on('departments');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_users');
+    }
+}

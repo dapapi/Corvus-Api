f74e9a0146b6aa802207eaeb67baea419a7f1d21
OmegaPenguin <xlf_129@163.com>
2018-11-04 20:10:46 +0800

    update TrailTransformer

--- app/Http/Transformers/TrailTransformer.php
+++ app/Http/Transformers/TrailTransformer.php
@@ -3,11 +3,13 @@
 namespace App\Http\Transformers;
 
 use App\Models\Trail;
+use App\Models\TrailArtist;
+use App\User;
 use League\Fractal\TransformerAbstract;
 
 class TrailTransformer extends TransformerAbstract
 {
-    protected $availableIncludes = ['principal', 'client', 'artist', 'contact', 'recommendation'];
+    protected $availableIncludes = ['principal', 'client', 'stars', 'contact', 'recommendations'];
 
     public function transform(Trail $trail)
     {
@@ -15,20 +17,64 @@ class TrailTransformer extends TransformerAbstract
             'id' => hashid_encode($trail->id),
             'title' => $trail->title,
             'brand' => $trail->brand,
-            'principal_id' => hashid_encode($trail->principal_id),
-            'client_id' => hashid_encode($trail->client_id),
-            'artist_id' => hashid_encode($trail->artist_id),
-            'contact_id' => hashid_encode($trail->contact_id),
+            'resource_type' => $trail->resource_type,
+            'fee' => $trail->fee / 100,
             'type' => $trail->type,
             'status' => $trail->status,
             'desc' => $trail->desc,
         ];
+
+        if ($trail->resource_type == Trail::PERSONAL) {
+            $resource = User::where('id', $trail->resource)->first();
+            $array['resource'] = [
+                'id' => hashid_encode($resource->id),
+                'name' => $resource->name,
+            ];
+        }
 
         return $array;
     }
 
     public function includePrincipal(Trail $trail)
     {
+        $principal = $trail->principal;
+        if (!$principal)
+            return null;
 
+        return $this->item($principal, new UserTransformer());
     }
+
+
+    public function includeClient(Trail $trail)
+    {
+        $client = $trail->client;
+        if (!$client)
+            return null;
+
+        return $this->item($client, new ClientTransformer());
+    }
+
+    public function includeContact(Trail $trail)
+    {
+        $contact = $trail->contact;
+        if (!$contact)
+            return null;
+
+        return $this->item($contact, new ContactTransformer());
+    }
+
+    public function includeRecommendations(Trail $trail)
+    {
+        $recommendations = $trail->recommendations;
+
+        return $this->collection($recommendations, new ArtistTransformer());
+    }
+
+//
+//    public function includeStars(Trail $trail)
+//    {
+//        $stars = $trail->stars;
+//
+//        return $this->collection($stars, new UserTransformer());
+//    }
 }

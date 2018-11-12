68c03a70565a65d4fd4ff684874cc5cb488d9d81
wyjson <wyjson@qq.com>
2018-11-12 14:11:59 +0800

    resource optimize

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -15,10 +15,11 @@ class ResourceController extends Controller
     {
 //        $payload = $request->all();
         $resources = Resource::all();
-
+        //要求关联模块里直接返回所有模块数据
         $metaArray = [
             'stars' => Star::all(),
             'projects' => Project::all(),
+            // TODO
         ];
         return $this->response->collection($resources, new ResourceTransformer())->setMeta($metaArray);
     }

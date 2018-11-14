b13c4c9a4f719782b1c0aa0ee982f90122a7b2cd
OmegaPenguin <xlf_129@163.com>
2018-11-14 14:57:53 +0800

    update in ClientTransformer

--- app/Http/Transformers/ClientTransformer.php
+++ app/Http/Transformers/ClientTransformer.php
@@ -7,6 +7,8 @@ use League\Fractal\TransformerAbstract;
 
 class ClientTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['principal', 'creator'];
+
     public function transform(Client $client)
     {
         $array = [
@@ -15,13 +17,29 @@ class ClientTransformer extends TransformerAbstract
             'grade' => $client->grade,
             'type' => $client->type,
             'address' => $client->address,
-            'principal' => $client->principal ? $client->principal->name : null,
-            'creator' => $client->creator->name,
             'size' => $client->size,
             'keyman' => $client->keyman,
             'desc' => $client->desc,
         ];
 
         return $array;
+    }
+
+    public function includePrincipal(Client $client)
+    {
+        $principal = $client->principal;
+        if (!$principal)
+            return null;
+
+        return $this->item($principal, new UserTransformer());
+    }
+
+    public function includeCreator(Client $client)
+    {
+        $creator = $client->creator;
+        if (!$creator)
+            return null;
+
+        return $this->item($creator, new UserTransformer());
     }
 }

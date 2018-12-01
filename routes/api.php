06d6c89990e940a68f2913313bf95dd213e094ef
laokai123 <765300725@qq.com>
2018-12-01 18:06:45 +0800

    任务没有列表

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,8 @@ $api->version('v1', ['middleware' => ['bindings', 'cors']], function ($api) {
         $api->put('/tasks/{task}', 'App\Http\Controllers\TaskController@edit');
         $api->post('/tasks/{task}/recover', 'App\Http\Controllers\TaskController@recoverRemove');
         $api->delete('/tasks/{task}', 'App\Http\Controllers\TaskController@remove');
+        $api->get('/tasksAll', 'App\Http\Controllers\TaskController@tasksAll');
+
 //            ->middleware('can:delete,task');
         $api->put('/tasks/{task}/status', 'App\Http\Controllers\TaskController@toggleStatus');
         $api->put('/tasks/{task}/time_cancel', 'App\Http\Controllers\TaskController@cancelTime');

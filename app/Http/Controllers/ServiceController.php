cdb6385a24d3b1bc67cd3cab1c074ad34da68aa6
OmegaPenguin <xlf_129@163.com>
2018-12-06 15:24:42 +0800

    fix service controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -16,6 +16,13 @@ use Webpatser\Uuid\Uuid;
 
 class ServiceController extends Controller {
 
+    public function cloudStorageToken()
+    {
+        $auth = new Auth(config('app.QINIU_ACCESS_KEY'), config('app.QINIU_SECRET_KEY'));
+        $upToken = $auth->uploadToken(config('app.QINIU_BUCKET'), null, 3600, null);
+        return $this->response->array(['data' => ['token' => $upToken]]);
+    }
+
     /**
      * @throws Exception
      */

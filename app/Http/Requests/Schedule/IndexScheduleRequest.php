805e5bcc700ee7235007e79f09223625cf406ef8
OmegaPenguin <xlf_129@163.com>
2018-12-11 16:45:28 +0800

    fix in IndexScheduleRequest

--- app/Http/Requests/Schedule/IndexScheduleRequest.php
+++ app/Http/Requests/Schedule/IndexScheduleRequest.php
@@ -25,7 +25,7 @@ class IndexScheduleRequest extends FormRequest
     {
         return [
             'start_date' => 'required|date',
-            'end_date' => 'required|date|after:start_date',
+            'end_date' => 'required|date|after_or_equal:start_date',
             'calendar_ids' => 'nullable|array',
             'material_ids' => 'nullable|array'
         ];

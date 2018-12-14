f021eb139dabb544d3592e099993be5df4fec150
laokai123 <765300725@qq.com>
2018-12-14 19:35:23 +0800

    功能页面渲染

--- routes/api.php
+++ routes/api.php
@@ -468,7 +468,7 @@ $api->version('v1', ['middleware' => ['bindings', 'cors']], function ($api) {
         //角色和用户关联
         $api->post('/console/relevancy/{role}','App\Http\Controllers\ConsoleController@setRoleUser');
         //功能列表
-        $api->get('/console/feature/{user}','App\Http\Controllers\ConsoleController@feature');
+        $api->get('/console/feature/{role}','App\Http\Controllers\ConsoleController@feature');
         //功能角色关联
         $api->post('/console/features/{role}','App\Http\Controllers\ConsoleController@featureRole');
         //增加数据权限

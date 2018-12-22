85891f3bec40c2f24efb185887e4c5025575c1a4
2802615124 <2802615124@qq.com>
2018-12-22 16:21:31 +0800

    更新 blogger

--- app/Http/Controllers/ReviewQuestionnaireController.php
+++ app/Http/Controllers/ReviewQuestionnaireController.php
@@ -77,7 +77,7 @@ class ReviewQuestionnaireController extends Controller {
             $number = $number == 0 ? 7 : $number; //如遇周末,将0换成7
             $diff_day = $number - 6; //求到周一差几天
             if($diff_day >= 0 ){
-                $diff_day = - (6 + $diff_day) ;
+                $diff_day = - (7 + $diff_day) ;
             }
             $deadline = date("Y-m-d 00:00:00",time() - ($diff_day * 60 * 60 * 24));
             $reviewquestionnairemodel->deadline = $deadline;

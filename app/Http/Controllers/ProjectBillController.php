f65f190e68551d289a026c9a04400bfd8d2d4f6f
2802615124 <2802615124@qq.com>
2018-12-22 10:28:36 +0800

    更新 blogger

--- app/Http/Controllers/ProjectBillController.php
+++ app/Http/Controllers/ProjectBillController.php
@@ -23,18 +23,27 @@ class ProjectBillController extends Controller
     {
         $payload = $request->all();
         $pageSize = $request->get('page_size', config('app.page_size'));
+        $payload['expense_type']=2;
         if($request->has('expense_type')){
-        $array['expense_type'] = $payload['expense_type'];
+            if($payload['expense_type']==1){
+
+                $array['expense_type'] = '收入';
+            }else if($payload['expense_type']==2){
+                $array['expense_type'] = '支出';
+            }else{
+                $array['expense_type'] = '';
+            }
+
         }
 
         if ($Blogger && $Blogger->id) {
-            $array['artist_name'] ='美豆爱厨房';
-          //  $array['artist_name'] = $Blogger->nickname;
+          //  $array['artist_name'] ='美豆爱厨房';
+            $array['artist_name'] = $Blogger->nickname;
         } else if ($project && $project->id) {
-            $array['project_kd_name'] ='美豆爱厨房';
-          //  $array['project_kd_name'] = $project->title;
+         //   $array['project_kd_name'] ='美豆爱厨房';
+            $array['project_kd_name'] = $project->title;
         }
-//        $sums =  ProjectBill::where($array)->select(DB::raw('sum(money) as sums'))->groupby('expense_type')->get();
+//       $sums =  ProjectBill::where($array)->select(DB::raw('sum(money) as sums'))->groupby('expense_type')->get();
         $projectbill = ProjectBill::where($array)->createDesc()->paginate($pageSize);
         return $this->response->paginator($projectbill, new ProjectBillTransformer());
     }

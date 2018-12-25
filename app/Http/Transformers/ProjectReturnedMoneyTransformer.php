8a81b10b548134536c4dacb11eaf669f2a73cdc3
2802615124 <2802615124@qq.com>
2018-12-25 16:09:40 +0800

    更新 项目

--- app/Http/Transformers/ProjectReturnedMoneyTransformer.php
+++ app/Http/Transformers/ProjectReturnedMoneyTransformer.php
@@ -20,7 +20,7 @@ class ProjectReturnedMoneyTransformer extends TransformerAbstract
                 'project_id' => hashid_encode($projectReturnedMoney->project_id),
 //                'creator_id' => hashid_encode($projectReturnedMoney->creator_id),
                 'principal_id' => hashid_encode($projectReturnedMoney->principal_id),
-                'issue_name' => '第'.$projectReturnedMoney->issue_name.'期',
+                'issue_name' => $projectReturnedMoney->issue_name,
                 'plan_returned_money' => $projectReturnedMoney->plan_returned_money,
                 'plan_returned_time' =>  date('Y-m-d',strtotime($projectReturnedMoney->plan_returned_time)),
           //      'project_returned_money_type_id' => $projectReturnedMoney->project_returned_money_type_id,

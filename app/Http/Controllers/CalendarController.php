28e80db2aaf45f194514da8b95766f843a439296
TimeFollowers <hello1576436427@163.com>
2018-12-27 12:02:23 +0800

    日历

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -31,19 +31,14 @@ class CalendarController extends Controller
     {
         // todo 按权限筛选
         $user = Auth::guard("api")->user();
-        $subquery = DB::table("calendars as c")->leftJoin('module_users as mu',function ($join){
-            $join->on('mu.moduleable_id','c.id')
-                ->where(DB::raw("mu.moduleable_type='".ModuleableType::CALENDAR."'"));
-        })->select('mu.user_id')->where(DB::raw("c.id=calendars.id"));
-
-            $calendars = Calendar::where(function ($query)use($user,$subquery) {
-                $query->where('privacy',Calendar::OPEN);
-                $query->orWhere('creator_id',$user->id);
-                $query->orWhere(function ($query)use ($user,$subquery){
-                    $query->where('privacy',Calendar::SECRET)->where(DB::raw("{$user->id} in ({$subquery->toSql()})"));
-            });
-            })->mergeBindings($subquery)
-                ->get();
+        $calendars  = Calendar::select('calendars.*','mu.user_id')->leftJoin('module_users as mu',function ($join){
+            $join->on('moduleable_id','calendars.id')
+                ->where('moduleable_type',ModuleableType::CALENDAR);
+        })->where(function ($query)use ($user){
+            $query->where('calendars.creator_id',$user->id);//创建人
+            $query->orWhere([['mu.user_id',$user->id],['calendars.privacy',Calendar::SECRET]]);//参与人
+            $query->orwhere('calendars.privacy',Calendar::OPEN);
+        })->get();
         return $this->response->collection($calendars, new CalendarTransformer());
     }
 

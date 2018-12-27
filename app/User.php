b0e6a247ca3d73d3c1766a10bee9e4ffa66e935c
OmegaPenguin <xlf_129@163.com>
2018-12-27 11:50:55 +0800

    fix for role in approval

--- app/User.php
+++ app/User.php
@@ -197,7 +197,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class, 'role_users');
     }
 
     public function skills()

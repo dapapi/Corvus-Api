8f491dafa285fb2905eef68dfafdec0e4f937f4c
OmegaPenguin <xlf_129@163.com>
2018-12-29 17:31:23 +0800

    fix in approval cancel

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contract extends Model
 {
-    use SoftDeletes;
     protected $fillable = [
         'contract_number',
         'form_instance_number',

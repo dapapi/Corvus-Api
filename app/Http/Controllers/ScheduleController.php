639aae7dae64faec6012e0867293078a9646b6d8
2802615124 <2802615124@qq.com>
2019-01-12 14:43:46 +0800

    更新线索

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -140,7 +140,7 @@ class ScheduleController extends Controller
             $subquery = DB::table("schedules as s")->leftJoin('module_users as mu',function ($join){
                 $join->on('mu.moduleable_id','s.id')
                     ->whereRaw("mu.moduleable_type='".ModuleableType::SCHEDULE."'")
-                    ->whereRaw("mu.type='".Schedule::SECRET."'");
+                    ->whereRaw("mu.type='".ModuleUserType::PARTICIPANT."'");
             })->select('mu.user_id');
 //->whereRaw("s.id=schedules.id")
             $schedules = Schedule::select('schedules.*')->where(function ($query)use ($payload,$user,$subquery){

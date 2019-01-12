d4887b602e1c6a17b0f75048d99362698ddb888b
OmegaPenguin <xlf_129@163.com>
2019-01-12 17:43:03 +0800

    fix in approval participant change pai

--- routes/api.php
+++ routes/api.php
@@ -618,7 +618,7 @@ $api->version('v1', ['middleware' => ['bindings', 'cors']], function ($api) {
         $api->get('/approvals/chains', 'App\Http\Controllers\ApprovalFlowController@getChains');
         $api->get('/approvals/{approval}/participants', 'App\Http\Controllers\ApprovalParticipantController@getFixedParticipants');
         $api->get('/approval_instances/{instance}/chains', 'App\Http\Controllers\ApprovalFlowController@getMergeChains');
-        $api->post('/approval_instances/{instance}/participant', 'App\Http\Controllers\ApprovalFlowController@getMergeChains');
+        $api->post('/approval_instances/{instance}/participant', 'App\Http\Controllers\ApprovalFlowController@changeParticipant');
         $api->put('/approval_instances/{instance}/agree', 'App\Http\Controllers\ApprovalFlowController@agree');
         $api->put('/approval_instances/{instance}/refuse', 'App\Http\Controllers\ApprovalFlowController@refuse');
         $api->put('/approval_instances/{instance}/transfer', 'App\Http\Controllers\ApprovalFlowController@transfer');

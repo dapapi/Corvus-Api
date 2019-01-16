cf0fc215ef6f8652935041e54b8778e87a07d195
OmegaPenguin <514910332@qq.com>
2019-01-16 10:01:45 +0800

    Star limit where

--- app/Models/Star.php
+++ app/Models/Star.php
@@ -95,7 +95,9 @@ class Star extends Model
 
     public function tasks()
     {
-        return $this->morphToMany(Task::class, 'resourceable','task_resources')->where('status',TaskStatus::NORMAL)->orderBy('created_at','desc')->limit(3);
+        //return $this->morphToMany(Task::class, 'resourceable','task_resources')->where('status',TaskStatus::NORMAL)->orderBy('created_at','desc')->limit(3);
+        return $this->morphToMany(Task::class, 'resourceable','task_resources')->orderBy('created_at','desc');
+
     }
 
     public function broker()

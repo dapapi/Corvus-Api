eaba63604817efab61d5a58185a960bab7712026
2802615124 <2802615124@qq.com>
2019-01-26 19:19:47 +0800

    修复 评分问卷

--- app/Http/Transformers/ReviewQuestionnaireShowTransformer.php
+++ app/Http/Transformers/ReviewQuestionnaireShowTransformer.php
@@ -60,8 +60,13 @@ class ReviewQuestionnaireShowTransformer extends TransformerAbstract{
             $arr  = ReviewAnswer::where('review_id', $reviewquestionnaire->id)->where('user_id',$user->id)->groupby('user_id')->get();
             if(count($arr)>0) {
                 $reviewanswer = ReviewAnswer::where('review_id', $reviewquestionnaire->id)->select('*', DB::raw('TRUNCATE(' . $sums[0]->sums . '/' . $count . ',2) as TRUNCATE'))->groupby('review_id');
+                return $this->collection($reviewanswer->get(), new ReviewAnswerSumTransformer());
+            }else{
+                 return null;
+                //return $this->collection($reviewanswer, new ReviewAnswerSumTransformer());
             }
-            return $this->collection($reviewanswer->get(), new ReviewAnswerSumTransformer());
+
+
         }else{
 
             $count =  count(ReviewAnswer::where('review_id', $reviewquestionnaire->id)->select('user_id',DB::raw('count(user_id) as counts'))->groupby('user_id')->get()->toArray());

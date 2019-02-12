b126a3e7384468b1e52fbd801e89ec888c27a0e0
2802615124 <2802615124@qq.com>
2019-02-12 17:25:38 +0800

    问卷的展示选项下的人

--- app/Http/Transformers/ReviewQuestionTransformer.php
+++ app/Http/Transformers/ReviewQuestionTransformer.php
@@ -63,6 +63,8 @@ class ReviewQuestionTransformer extends TransformerAbstract
                 {
                     $arr = array();
                     $arrdate = null;
+                }else{
+                    $arrdate = true;
                 }
             }
         }

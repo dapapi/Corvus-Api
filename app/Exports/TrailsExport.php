0b55ae5a851608cb7dae62a9d9c3d9a2bf24186f
2802615124 <2802615124@qq.com>
2019-02-20 16:19:29 +0800

    1.2.2019.02.20 bug_id = 1172 线索导出

--- app/Exports/TrailsExport.php
+++ app/Exports/TrailsExport.php
@@ -25,30 +25,36 @@ class TrailsExport implements FromQuery, WithMapping, WithHeadings
     {
         $request = $this->request;
         $payload =  $request->all();
-        $trails = Trail::where(function ($query) use ($request, $payload) {
+        return $trails = Trail::where(function ($query) use ($request, $payload) {
             if ($request->has('keyword') && $payload['keyword'])
                 $query->where('title', 'LIKE', '%' . $payload['keyword'] . '%');
             if ($request->has('status') && !is_null($payload['status']))
-                $query->where('type', $payload['status']);
+                $query->where('progress_status', $payload['status']);
             if ($request->has('principal_ids') && $payload['principal_ids']) {
                 $payload['principal_ids'] = explode(',', $payload['principal_ids']);
                 foreach ($payload['principal_ids'] as &$id) {
                     $id = hashid_decode((int)$id);
                 }
                 unset($id);
-                $query->query()->whereIn('principal_id', $payload['principal_ids']);
+                $query->whereIn('principal_id', $payload['principal_ids']);
             }
-        });
-        return $trails->searchData()->poolType()
-                //->orderBy('created_at', 'desc')
-                ->leftJoin('operate_logs',function($join){
-                    $join->on('trails.id','operate_logs.logable_id')
-                        ->where('logable_type',ModuleableType::TRAIL)
-                        ->where('operate_logs.method','4');
-                })->groupBy('trails.id')
-                ->orderBy('up_time', 'desc')->orderBy('trails.created_at', 'desc')->select(['trails.id','title','brand','principal_id','industry_id','client_id','contact_id','creator_id',
-                    'type','trails.status','priority','cooperation_type','lock_status','lock_user','lock_at','progress_status','resource','resource_type','take_type','pool_type','receive','fee','desc',
-                    'trails.updated_at','trails.created_at','pool_type','take_type','receive',DB::raw("max(operate_logs.updated_at) as up_time")]);
+            if($request->has('type') && $payload['type'])
+                $query->where('type',$payload['type']);
+
+        })
+            ->searchData()->poolType()
+            //->orderBy('created_at', 'desc')
+            ->leftJoin('operate_logs',function($join){
+                $join->on('trails.id','operate_logs.logable_id')
+                    ->where('logable_type',ModuleableType::TRAIL)
+                    ->where('operate_logs.method','4');
+            })->groupBy('trails.id')
+            ->orderBy('up_time', 'desc')->orderBy('trails.created_at', 'desc')->select(['trails.id','title','brand','principal_id','industry_id','client_id','contact_id','creator_id',
+                'type','trails.status','priority','cooperation_type','lock_status','lock_user','lock_at','progress_status','resource','resource_type','take_type','pool_type','receive','fee','desc',
+                'trails.updated_at','trails.created_at','pool_type','take_type','receive',DB::raw("max(operate_logs.updated_at) as up_time")]);
+//                        $sql_with_bindings = str_replace_array('?', $trails->getBindings(), $trails->toSql());
+////
+//        dd($sql_with_bindings);
     }
 
     /**

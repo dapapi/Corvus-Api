bc5b5d3c268518be95bc56b503269d7b8838d039
TimeFollowers <hello1576436427@163.com>
2019-02-28 18:59:08 +0800

    1.2.20190227 bug_id = 1194 增加最后跟进时间

--- app/Http/Controllers/StarController.php
+++ app/Http/Controllers/StarController.php
@@ -830,7 +830,7 @@ class StarController extends Controller
         $stars = $query->where(function ($query) use ($payload) {
             FilterReportRepository::getTableNameAndCondition($payload,$query);
         });
-        $stars->select('stars.id','stars.name','stars.source','stars.created_at','stars.updated_at','stars.sign_contract_status');
+        $stars->select('stars.id','stars.name','stars.source','stars.created_at','stars.updated_at','stars.sign_contract_status',DB::raw("max(operate_logs.updated_at) as up_time"));
         $stars = $stars->orderBy('stars.created_at', 'desc')->groupBy('stars.id')->paginate($pageSize);
 
         return $this->response->paginator($stars, new StarTransformer(!$all));

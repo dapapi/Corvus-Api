52ead7cc1579a99a54917e8f233b6334c3361d1b
TimeFollowers <hello1576436427@163.com>
2019-03-07 11:16:55 +0800

    1.2.20190306 bug_id = 1256修复user/my 更改权限存放位置

--- app/Http/Transformers/UserTransformer.php
+++ app/Http/Transformers/UserTransformer.php
@@ -65,7 +65,8 @@ class UserTransformer extends TransformerAbstract
             'work_email' => $user->work_email,//'工作邮箱',
             'disable' => $user->disable,
             'entry_status' => $user->entry_status,
-            'organization_id'   =>  $user->department()->first() == null ? 0 : $user->department()->first()->company_id
+            'organization_id'   =>  $user->department()->first() == null ? 0 : $user->department()->first()->company_id,
+            'power' =>  $user->power,
         ];
 
         $companyInfo = DB::table('department_user as du')//

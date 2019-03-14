1cface52a408b364c8fa390219b976d63dc4126d
TimeFollowers <hello1576436427@163.com>
2019-03-14 12:27:02 +0800

    1.3.20190313 艺人隐私字段

--- app/Traits/PrivacyFieldTrait.php
+++ app/Traits/PrivacyFieldTrait.php
@@ -28,20 +28,21 @@ trait PrivacyFieldTrait
     {
         //如果不存在id，则判断为新建不加限制
         $id = $this->getAttribute('id');
-        if (!$id){//不是隐私字段允许修改
-            return $value;
-        }
+
         //判断字段是否是隐私字段
         $privacy_field_list = DataDictionarie::getPrivacyFieldList();
-        if (!in_array($module.".".$key,$privacy_field_list)){//如果该字段在隐私字段内,则判断是否有权限查看
-            return $value;
+        if (!in_array($module.".".$key,$privacy_field_list)){//如果该字段不在隐私字段内
+            $this->attributes[$key] = $value;
+        }else{
+            $user = Auth::guard("api")->user();
+            $has_power = PrivacyUserRepository::has_power($module,$key,$id,$user->id);//判断是否有权限查看该字段
+            if($this->creator_id == $user->id || $has_power){//创建人可以修改有权限的可以修改
+                $this->attributes[$key] = $value;//有权限则修改
+            }else{
+                $this->attributes[$key] = $this->getOriginal($key);
+            }
         }
-        $user = Auth::guard("api")->user();
-        $has_power = PrivacyUserRepository::has_power($module,$key,$id,$user->id);//判断是否有权限查看该字段
-        if($this->creator_id == $user->id || $has_power){//创建人可以修改有权限的可以修改
-            return $this->getOriginal($key);
-        }
-        return $value;//有权限则修改
+
     }
 
     /**
@@ -71,10 +72,11 @@ trait PrivacyFieldTrait
         $user = Auth::guard("api")->user();
         $id = $this->attributes['id'];
         $has_power = PrivacyUserRepository::has_power($module,$key,$id,$user->id);//判断是否有权限查看该字段
-        if (!$has_power){ //没权限则不修改
-            return "xxxxxx";
-        }
 
-        return $this->attributes[$key];//有权限则修改
+        if($this->creator_id == $user->id || $has_power){//创建人可以修改有权限的可以修改
+            return $this->attributes[$key];//有权限则修改
+        }
+        return "xxxxxx";
+
     }
 }

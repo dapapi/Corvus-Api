26b5ebc8b4cbf13e22a2ca1de4d4c06dbf3639f0
OmegaPenguin <xlf_129@163.com>
2019-03-22 17:49:46 +0800

    add softdeletes for approval group

--- app/Models/ApprovalGroup.php
+++ app/Models/ApprovalGroup.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use App\Models\ApprovalForm\ApprovalForm;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ApprovalGroup extends Model
 {
     protected $table = 'approval_form_groups';
+    use SoftDeletes;
 
     protected $fillable = [
         'name',

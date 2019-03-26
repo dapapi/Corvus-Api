77b0d08e041d168522349df65fd8e22175733d95
2802615124 <2802615124@qq.com>
2019-03-26 15:25:34 +0800

    1.3.20190325     修改日历返回字段

--- app/Http/Transformers/CalendarTransformer.php
+++ app/Http/Transformers/CalendarTransformer.php
@@ -16,7 +16,7 @@ class CalendarTransformer extends TransformerAbstract
             'id' => hashid_encode($calendar->id),
             'title' => $calendar->title,
             'color' => $calendar->color,
-            'principal_id' => hashid_encode($calendar->principal_id),   //
+            'principal_id' => empty($calendar->principal_id)? null:hashid_encode($calendar->principal_id),   //
             'starable_type' => $calendar->starable_type,   
             'privacy' => $calendar->privacy,
         ];

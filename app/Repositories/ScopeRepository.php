12535d23bffed44bdb04a41c320e201f9671aa13
TimeFollowers <hello1576436427@163.com>
2019-03-28 16:13:37 +0800

    权限修改

--- app/Repositories/ScopeRepository.php
+++ app/Repositories/ScopeRepository.php
@@ -389,8 +389,8 @@ class ScopeRepository
                 ],
                 'task' => [
                     'add' => ['method' => 'post', 'uri' => 'tasks'],
-                    'export' => ['method' => 'post', 'uri' => 'tasks/export'],
-                    'import' => ['method' => 'post', 'uri' => 'tasks/import']
+//                    'export' => ['method' => 'post', 'uri' => 'tasks/export'],
+//                    'import' => ['method' => 'post', 'uri' => 'tasks/import']
                 ],
                 'client' => [
                     'add' => ['method' => 'post', 'uri' => 'clients'],

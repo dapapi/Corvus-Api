598ad54f4048182287efbcfe1e25c207faf11df3
TimeFollowers <hello1576436427@163.com>
2019-04-01 22:19:31 +0800

    增加假分页

--- app/Http/Controllers/StarController.php
+++ app/Http/Controllers/StarController.php
@@ -935,7 +935,21 @@ class StarController extends Controller
             $res[$key]['last_follow_up_at'] = $star->last_follow_up_at;
 
         }
-        return $res;
+        $res['meta'] = [
+        "meta"=> [
+            "pagination"=> [
+                "total"=> 576,
+                "count"=> 15,
+                "per_page"=> 15,
+                "current_page"=> 1,
+                "total_pages"=> 39,
+                "links"=> [
+                    "next"=> "http://corvus.cn/stars/filter?page=2"
+                ],
+            ]
+        ]
+    ];
+        return ["data" => $res];
     }
     public function getStarById(Star $star)
     {

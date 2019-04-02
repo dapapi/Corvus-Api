855d4ecd4b0bf4932335543b81087011f66b70bf
TimeFollowers <hello1576436427@163.com>
2019-04-02 20:35:08 +0800

    博主列表

--- app/Repositories/BloggerRepository.php
+++ app/Repositories/BloggerRepository.php
@@ -44,21 +44,24 @@ class BloggerRepository
      * @author lile
      * @date 2019-04-02
      */
-    public static function getBloggerList($condition,$search_field)
+    public static function getBloggerList($condition,$search_field,$pageSize,$page)
     {
         if ($condition == null){
             $condition['where'] = null;
             $condition['placeholder'] = [];
         }
+
         $placeholder = $condition['placeholder'];
         $where = Blogger::powerConditionSql();
-        //第一次进入时的sql
 
+        $offset = ($page-1) * $pageSize;
+
+        //第一次进入时的sql
 
         if (in_array('module_users.user_id',$search_field) ||in_array('department_user.department_id',$search_field) ) {//根据经理人，部门查询的sql
             $sql = <<<AAA
             select
-                bloggers.nickname,bloggers.id,bloggers.type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status,group_concat(users.name) as publicity_user_names
+                bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status,group_concat(users.name) as publicity_user_names
             from bloggers
             left join module_users on module_users.moduleable_id = bloggers.id and module_users.moduleable_type = :moduleable_type and module_users.type = :module_users_type
             left join department_user on department_user.user_id = module_users.user_id
@@ -66,17 +69,18 @@ class BloggerRepository
             left join blogger_types on blogger_types.id = bloggers.type_id
                 where (1=1 {$where}) {$condition['where']} 
                 group by bloggers.id
-            limit 0,10
+            limit {$offset},{$pageSize}
 AAA;
             $placeholder[":moduleable_type"] = ModuleableType::BLOGGER;
             $placeholder[":module_users_type"] = ModuleUserType::PUBLICITY;
         }else{
             $sql = <<<AAA
             select 
-              bloggers.nickname,bloggers.id,bloggers.type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status
+              bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status
             from bloggers
             left join blogger_types on blogger_types.id = bloggers.type_id
-            where (1 = 1 {$where})  {$condition['where']} limit 0,10
+            where (1 = 1 {$where})  {$condition['where']}
+            limit {$offset},{$pageSize}
 AAA;
         }
         return DB::select($sql,$placeholder);

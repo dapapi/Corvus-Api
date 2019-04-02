1f544b2ff523e8e4ae9d996c20c560ff1cd13a7f
yuchao <765300725@qq.com>
2019-04-02 15:02:07 +0800

    任务详情

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -185,7 +185,7 @@ class TaskController extends Controller
         $my = $request->get('my',0);
         $pageSize = $request->get('page_size', config('app.page_size'));
 
-        $query = Task::select('tasks.id','tasks.title as task_name','tasks.status','tasks.resource_name','tasks.resource_type','tasks.principal_name','tasks.type_name','tasks.adj_id');
+        $query = Task::select('tasks.id','tasks.title','tasks.status','tasks.resource_name','tasks.resource_type','tasks.principal_name','tasks.type_name','tasks.adj_id','tasks.end_at');
 
         switch ($my) {
             case 2://我参与
@@ -484,6 +484,33 @@ class TaskController extends Controller
         $request = $this->response->paginator($tasks, new TaskTransformer());
         $request->addMeta("complete_count",$complete_count);
         return $request;
+    }
+
+
+    public function showDemo(Task $task,ScopeRepository $repository)
+    {
+        // 操作日志
+        $operate = new OperateEntity([
+            'obj' => $task,
+            'title' => null,
+            'start' => null,
+            'end' => null,
+            'method' => OperateLogMethod::LOOK,
+        ]);
+        event(new OperateLogEvent([
+            $operate,
+        ]));
+        //登录用户对线索编辑权限验证
+        try{
+            $user = Auth::guard("api")->user();
+            //获取用户角色
+            $role_list = $user->roles()->pluck('id')->all();
+            $repository->checkPower("tasks/{id}",'put',$role_list,$task);
+            $task->power = "true";
+        }catch (Exception $exception){
+            $task->power = "false";
+        }
+        return $this->response()->item($task, new TaskTransformer());
     }
 
     public function show(Task $task,ScopeRepository $repository)

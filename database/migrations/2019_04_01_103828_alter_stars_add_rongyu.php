0ae34c3ba48a646cc7ab8e47da6d4940c0434053
TimeFollowers <hello1576436427@163.com>
2019-04-02 10:57:52 +0800

    优化

--- database/migrations/2019_04_01_103828_alter_stars_add_rongyu.php
+++ database/migrations/2019_04_01_103828_alter_stars_add_rongyu.php
@@ -19,6 +19,7 @@ class AlterStarsAddRongyu extends Migration
             $table->dateTime("last_updated_at")->comment('最近更新时间，同步于operate_logs表')->nullable();
             $table->dateTime("last_follow_up_at")->comment('最近跟进时间，同步于operate_logs表')->nullable();
             $table->dateTime('contract_start_date')->comment('合同开始时间')->nullable();
+            $table->dateTime('contract_end_date')->comment('合同截止日期')->nullable();
         });
     }
 

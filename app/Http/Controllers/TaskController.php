28717d601424b2cfa3256dc2712c75b32cfe88c1
yuchao <765300725@qq.com>
2019-04-05 03:32:14 +0800

    任务修改

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1254,7 +1254,6 @@ class TaskController extends Controller
     public function taskEdit(TaskUpdateRequest $request, Task $task)
     {
         $payload = $request->all();
-
         $oldTask = clone $task;
         $user = Auth::guard('api')->user();
 

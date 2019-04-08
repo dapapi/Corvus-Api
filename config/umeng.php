cb6c1542d0a8de21afc1a4f12230d9dee8a9ac36
TimeFollowers <hello1576436427@163.com>
2019-04-08 20:20:41 +0800

    umeng

---
+++ config/umeng.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ios_app_key'                => '',
+    'ios_app_master_secret'     => '',
+    'android_app_key'            => '5cab28b90cafb23deb000b87',
+    'android_app_master_secret' => 'hjqktncw7xwedzmgh2mrcjwii2vgjpze',
+    'production_mode'                =>  env('UMENG_MODE', false)
+];

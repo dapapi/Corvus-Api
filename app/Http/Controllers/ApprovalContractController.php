2e77b7c0109811279cbf104dba410622d322f69f
yuchao <765300725@qq.com>
2019-04-12 14:23:17 +0800

    合同列表头像

--- app/Http/Controllers/ApprovalContractController.php
+++ app/Http/Controllers/ApprovalContractController.php
@@ -709,6 +709,9 @@ class ApprovalContractController extends Controller
             })
             ->join('contracts as cs', function ($join) {
                 $join->on('afb.form_instance_number', '=', 'cs.form_instance_number');
+            })
+            ->join('users', function ($join) {
+                $join->on('cs.creator_id', '=', 'users.id');
             })->contractSearchData();
 
         $res = $data->join('projects as ps', function ($join) {
@@ -717,7 +720,7 @@ class ApprovalContractController extends Controller
             ->where('cs.project_id', $projects)
             ->where('afb.form_status', 232)
             ->orderBy('cs.created_at', 'desc')
-            ->select('afb.form_instance_number', 'cs.contract_number', 'cs.title', 'af.name as form_name', 'cs.creator_name', DB::raw("DATE_FORMAT(cs.created_at,'%Y-%m-%d %h:%i') as created_at"), 'afb.form_status', 'cs.stars', 'cs.star_type', 'cs.contract_money', 'cs.type')->get()->toArray();
+            ->select('afb.form_instance_number', 'cs.contract_number', 'cs.title', 'af.name as form_name', 'cs.creator_name', DB::raw("DATE_FORMAT(cs.created_at,'%Y-%m-%d %h:%i') as created_at"), 'afb.form_status', 'cs.stars', 'cs.star_type', 'cs.contract_money', 'cs.type', 'users.icon_url')->get()->toArray();
 
 
         $dataInfo = json_decode(json_encode($res), true);

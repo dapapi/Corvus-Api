f0d876e7d611f65ca57441ba3ede4880b8f31117
TimeFollowers <hello1576436427@163.com>
2019-04-17 11:57:40 +0800

    调整签约中的艺人博主，权限控制

--- app/Repositories/BloggerRepository.php
+++ app/Repositories/BloggerRepository.php
@@ -46,68 +46,68 @@ class BloggerRepository
      * @author lile
      * @date 2019-04-02
      */
-    public static function getBloggerList($condition,$search_field,$pageSize,$page)
-    {
-        if ($condition == null){
-            $condition['where'] = null;
-            $condition['placeholder'] = [];
-        }
-
-        $placeholder = $condition['placeholder'];
-        $where = Blogger::powerConditionSql();
-
-        $offset = ($page-1) * $pageSize;
-
-
-        //第一次进入时的sql
-
-        if (in_array('module_users.user_id',$search_field) ||in_array('department_user.department_id',$search_field) ) {//根据经理人，部门查询的sql
-            $sql = <<<AAA
-            select
-                bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status,group_concat(users.name) as publicity_user_names
-            from bloggers
-            left join module_users on module_users.moduleable_id = bloggers.id and module_users.moduleable_type = :moduleable_type and module_users.type = :module_users_type
-            left join department_user on department_user.user_id = module_users.user_id
-            left join users on department_user.user_id = users.id
-            left join blogger_types on blogger_types.id = bloggers.type_id
-            where (1 = 1 {$where})  {$condition['where']}
-                group by bloggers.id
-AAA;
-            $placeholder[":moduleable_type"] = ModuleableType::BLOGGER;
-            $placeholder[":module_users_type"] = ModuleUserType::PUBLICITY;
-        }else{
-            $sql = <<<AAA
-            select
-              bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status
-            from bloggers
-            left join blogger_types on blogger_types.id = bloggers.type_id
-            where (1 = 1 {$where})  {$condition['where']}
-AAA;
-
-        }
-
-        $count = DB::select("select count(temp.id) as total from ({$sql}) as temp",$placeholder);
-        $total = $count[0]->total;
-        $sql .= " limit {$offset},{$pageSize}";
-        $data = DB::select($sql,$placeholder);
-        $total_page = ceil($total / $pageSize);
-        $meta = [
-            "pagination"=> [
-                "total"=> $total,
-                "count"=> count($data),
-                "per_page"=> $page - 1 == 0 ? 1: $page-1,
-                "current_page"=> $page,
-                "total_pages"=> $total_page,
-                "links"=> [
-                    "next"=> "http://corvus.cn/stars/filter?page=2"
-                ],
-            ]
-        ];
-        return [
-            $data,
-            $meta,
-        ];
-    }
+//    public static function getBloggerList($condition,$search_field,$pageSize,$page)
+//    {
+//        if ($condition == null){
+//            $condition['where'] = null;
+//            $condition['placeholder'] = [];
+//        }
+//
+//        $placeholder = $condition['placeholder'];
+//        $where = Blogger::powerConditionSql();
+//
+//        $offset = ($page-1) * $pageSize;
+//
+//
+//        //第一次进入时的sql
+//
+//        if (in_array('module_users.user_id',$search_field) ||in_array('department_user.department_id',$search_field) ) {//根据经理人，部门查询的sql
+//            $sql = <<<AAA
+//            select
+//                bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status,group_concat(users.name) as publicity_user_names
+//            from bloggers
+//            left join module_users on module_users.moduleable_id = bloggers.id and module_users.moduleable_type = :moduleable_type and module_users.type = :module_users_type
+//            left join department_user on department_user.user_id = module_users.user_id
+//            left join users on department_user.user_id = users.id
+//            left join blogger_types on blogger_types.id = bloggers.type_id
+//            where (1 = 1 {$where})  {$condition['where']}
+//                group by bloggers.id
+//AAA;
+//            $placeholder[":moduleable_type"] = ModuleableType::BLOGGER;
+//            $placeholder[":module_users_type"] = ModuleUserType::PUBLICITY;
+//        }else{
+//            $sql = <<<AAA
+//            select
+//              bloggers.nickname,bloggers.id,blogger_types.name as type,bloggers.sign_contract_status,bloggers.weibo_fans_num,bloggers.type_id,bloggers.sign_contract_at,bloggers.terminate_agreement_at,bloggers.created_at,bloggers.last_follow_up_at,bloggers.communication_status
+//            from bloggers
+//            left join blogger_types on blogger_types.id = bloggers.type_id
+//            where (1 = 1 {$where})  {$condition['where']}
+//AAA;
+//
+//        }
+//
+//        $count = DB::select("select count(temp.id) as total from ({$sql}) as temp",$placeholder);
+//        $total = $count[0]->total;
+//        $sql .= " limit {$offset},{$pageSize}";
+//        $data = DB::select($sql,$placeholder);
+//        $total_page = ceil($total / $pageSize);
+//        $meta = [
+//            "pagination"=> [
+//                "total"=> $total,
+//                "count"=> count($data),
+//                "per_page"=> $page - 1 == 0 ? 1: $page-1,
+//                "current_page"=> $page,
+//                "total_pages"=> $total_page,
+//                "links"=> [
+//                    "next"=> "http://corvus.cn/stars/filter?page=2"
+//                ],
+//            ]
+//        ];
+//        return [
+//            $data,
+//            $meta,
+//        ];
+//    }
 
     public static function getBloggerList2($search_field)
     {

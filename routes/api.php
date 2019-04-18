412b3d45089d6801139a046465c714545445ad43
OmegaPenguin <xlf_129@163.com>
2019-04-18 10:57:07 +0800

    fix for approval

--- routes/api.php
+++ routes/api.php
@@ -862,7 +862,7 @@ $api->version('v1', ['middleware' => ['bindings', 'cors']], function ($api) {
         $api->post('/aims', 'App\Http\Controllers\AimController@store');
         $api->get('/aims/all', 'App\Http\Controllers\AimController@all');
         $api->get('/aims/{aim}', 'App\Http\Controllers\AimController@detail');
-        $api->get('/aims/{aim}/count', 'App\Http\Controllers\AimController@count');
+        $api->get('/aims/count', 'App\Http\Controllers\AimController@count');
         $api->put('/aims/{aim}', 'App\Http\Controllers\AimController@edit');
         $api->post('/aims/{aim}/project', 'App\Http\Controllers\AimController@relateProject');
         $api->put('/aims/{aim}/status', 'App\Http\Controllers\AimController@changeStatus');
